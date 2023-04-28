498327154136a433d09dff6fc7c007d7367c1c45
David RACODON <david.racodon@gmail.com>
2023-04-28 20:24:35 +0200

    Set LTS compatibility

--- fpga-metrics-its/plugin/src/test/java/com/lintyservices/sonar/plugins/fpgametrics/its/Tests.java
+++ fpga-metrics-its/plugin/src/test/java/com/lintyservices/sonar/plugins/fpgametrics/its/Tests.java
@@ -35,7 +35,7 @@ public class Tests {
 
   @ClassRule
   public static final Orchestrator ORCHESTRATOR = Orchestrator.builderEnv()
-    .setSonarVersion("10.0.0.68432")
+    .setSonarVersion(System.getProperty("sonar.runtimeVersion", "LATEST_RELEASE"))
     .addPlugin(FileLocation.byWildcardMavenFilename(new File("../../sonar-fpga-metrics-plugin/target"), "sonar-fpga-metrics-plugin-*.jar"))
     .build();
 }
